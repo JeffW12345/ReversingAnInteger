71a311e670fc37a82fd04c53bc402168999e28ec
JeffW12345 <59927061+JeffW12345@users.noreply.github.com>
2021-10-04 21:21:03 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,17 +9,17 @@ public class Test {
 		System.out.println(reverseSecondApproach(input));
 	}
 
-	private static int reverse(int input) {
+	private static long reverse(int input) {
 		Integer inputAsInteger = input;
 		String asStr = inputAsInteger.toString();
 		String inReverse = "";
 		for (int i = asStr.length() - 1; i > -1; i--) {
 			inReverse += asStr.charAt(i);
 		}
-		return Integer.parseInt(inReverse);
+		return Long.parseLong(inReverse);
 	}
 	
-	private static int reverseSecondApproach(int input) {
+	private static long reverseSecondApproach(int input) {
 		long newNum = 0;
 		while(input > 0) {
 			newNum *= 10;
